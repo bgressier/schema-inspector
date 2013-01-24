5dad4e5d97a1fb5e856fce7065ec0715aa4ab13c
Benjamin GRESSIER <benjamin.gressier@epitech.eu>
2013-01-24 12:49:47 +0100

    Uncommented tests

--- test/test.js
+++ test/test.js
@@ -2,6 +2,6 @@ var validation = require('./validation_test').validation;
 var sanitization = require('./sanitization_test').sanitization;
 var generator = require('./generator_test').generator;
 
-// suite('Validation', validation);
+suite('Validation', validation);
 suite('Sanitization', sanitization);
-// suite('Generator', generator);
+suite('Generator', generator);
