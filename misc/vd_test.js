d7153c7e723f90c0b524cc29c31d9a46020ca733
Benjamin GRESSIER <benjamin.gressier@epitech.eu>
2013-02-01 20:29:00 +0100

    Validation + Sanitization: Added extend, reset & remove functions to define default custom fields

--- misc/vd_test.js
+++ misc/vd_test.js
@@ -1,73 +1,27 @@
-if (typeof require === 'function') {
-	var SchemaInspector = require('../');
-}
+var si = require('../');
 
-(function (SchemaInspector, $) {
-
-	var display = typeof alert === 'function' ? alert : console.log;
-
-	function format(json) {
-		return JSON.stringify(json, null, 2)
-		.replace(/\n/g, '<br>')
-		.replace(/ /g, '&nbsp;&nbsp;')
-		.replace(/("[^"]*"):/g, '<font color="blue">$1</font>:')
-		.replace(/:(&nbsp;)+("[^"]*")(,?)/g, ':$1<font color="green">$2</font>$3')
-		.replace(/:(&nbsp;)+(\d+)(,?)/g, ':$1<font color="violet">$2</font>$3')
-		.replace(/:(&nbsp;)+(true|false|null)(,?)/g, ':$1<font color="red">$2</font>$3')
-		.replace(/:(&nbsp;)+(undefined)(,?)/g, ':$1<font color="grey">$2</font>$3');
+si.Validation.extend({
+	divisibleBy: function (schema, candidate) {
+		var dvb = schema.$divisibleBy;
+		if (candidate % dvb) {
+			this.report('must be divisible by ' + dvb);
+		}
 	}
+});
 
 var schema = {
 	type: 'array',
-	minLength: 1,
-	items: [{
-		type: [ 'string', 'object' ],
-		properties: {
-			merchantId: {
-				type: [ 'integer', 'string' ],
-				optional: true,
-				alias: 'merchant Id'
-			},
-			id: {
-				type: [ 'integer', 'string'],
-				optional: true,
-				alias: 'id'
-			},
-			mktpAlias: {
-				type: 'string',
-				optional: true,
-				alias: 'marketplace alias'
-			}
-		}
-	}]
+	items: {
+		type: 'number', $divisibleBy: 5
+	}
 };
 
-var obj = [ '50ffc1baad86e22212000007' ];
+var obj = [ 0, 5, 10, 15, 17, 20];
 
-	// ---------------------------------------------------------------------------
-	var vdr = SchemaInspector.newValidation(schema);
+// -----------------------------------------------------------------------------
+var vdr = si.newValidation(schema);
 
-	vdr.validate(obj, function (err, r) {
-		console.log(r);
-		console.log(r.format());
-	});
-
-return setTimeout(function () {}, 2000);
-	var r = SchemaInspector.validate(obj);
-
-	var done = function () {
-		var html = '<p>'
-			+ 'Validation = '
-			+ format(r)
-			+ '</p>';
-		$('div.resultValidation').html(html);
-	};
-
-	if ($ !== null) {
-		$(done);
-	}
-	else {
-		console.log(r);
-		console.log(r.format());
-	}
-}).call(this, SchemaInspector, typeof jQuery !== 'undefined' ? jQuery : null);
+vdr.validate(obj, function (err, r) {
+	console.log(r);
+	console.log(r.format());
+});
